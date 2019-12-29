b344b3ec6dbd117dc66fce78d0c5947dfe1df893
Aashray <aashray@uw.edu>
2019-12-28 17:34:33 -0800

    finished part 3

--- Set1/SingleByteXorCipher.py
+++ Set1/SingleByteXorCipher.py
@@ -6,18 +6,35 @@ alphabet = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # use ocurrences of each letter in output of each
 # XOR to determine character which will give expected output
-char_weights = {'e': 0.1041442, 
-                't': 0.1041442,
-                'a': 0.0651738,
-                'o': 0.0596302,
-                'i': 0.0558094,
-                'n': 0.0564513,
-                's': 0.0515760,
-                'h': 0.0492888,
-                'r': 0.0497563,
-                'd': 0.0349835,
-                'l': 0.0331490,
-                'u': 0.0225134}
+char_weights = {
+    'a': 0.0651738,
+    'b': 0.0124248, 
+    'c': 0.0217339, 
+    'd': 0.0349835, 
+    'e': 0.1041442, 
+    'f': 0.0197881, 
+    'g': 0.0158610,
+    'h': 0.0492888, 
+    'i': 0.0558094, 
+    'j': 0.0009033, 
+    'k': 0.0050529, 
+    'l': 0.0331490, 
+    'm': 0.0202124, 
+    'n': 0.0564513,
+    'o': 0.0596302, 
+    'p': 0.0137645, 
+    'q': 0.0008606, 
+    'r': 0.0497563, 
+    's': 0.0515760, 
+    't': 0.0729357, 
+    'u': 0.0225134,
+    'v': 0.0082903, 
+    'w': 0.0171272, 
+    'x': 0.0013692, 
+    'y': 0.0145984, 
+    'z': 0.0007836, 
+    ' ': 0.1918182
+}
 
 def ByteXor(input):
     outputs = []
@@ -29,9 +46,8 @@ def ByteXor(input):
             output_i.append(xorChar)
         res = "".join(output_i)
         outputs.append(res)
-        print(res)
         output_i = []
-    evaluteOutputs(outputs)
+    return evaluteOutputs(outputs)
 
 def evaluteOutputs(outputs):
     scores = []
@@ -42,7 +58,8 @@ def evaluteOutputs(outputs):
                 score += char_weights[char]
         score = score / len(output)
         scores.append(score)
-    res = [x for x,_ in sorted(zip(outputs, scores))]
-    print(res[0:5])
+    res = [x for x,y in sorted(zip(outputs, scores), key=lambda x: x[1], reverse=True)]
+    return res[0]
 
-ByteXor(input)
+res = ByteXor(input)
+assert res == "Cooking MC's like a pound of bacon"
